ba5bc198fcec3a19593edf8deb76a9125a5f7021
shivam <shivam@sinch.com>
2026-01-06 19:26:35 +0530

    Added auth, resolved public path learned about browser history and learned auth

---
+++ auth/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+// import Pricing from './components/Pricing';
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'au',
+});
+export default ({ history, onSignIn }) => {
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/auth/signin">
+            <Signin onSignIn={onSignIn} />
+          </Route>
+          <Route exact path="/auth/signup">
+            <Signup onSignIn={onSignIn} />
+          </Route>
+        </Switch>
+      </Router>
+    </StylesProvider>
+  );
+};
