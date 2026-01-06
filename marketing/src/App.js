a228483b3da85879fe2c6fc73437db8541251ddc
shivam <shivam@sinch.com>
2026-01-06 15:00:32 +0530

    Added container to child and child to container navigation

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { Switch, Route, Router } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
-
-export default () => {
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+export default ({ history }) => {
   return (
-    <StylesProvider>
-      <Router>
+    <StylesProvider generateClassName={generateClassName}>
+      <Router history={history}>
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/pricing" component={Pricing} />
